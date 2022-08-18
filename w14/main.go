53056370a6c7c11391f2de60878c42c8ca9c6ac1
Vitaly Bogryakov <vitjrbog@gmail.com>
2022-08-18 16:45:53 +0500

    Completed the 14th assignment

---
+++ w14/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	// объявляем несколько переменных разных типов
+	var i int
+	var s string
+	var b bool
+	ch := make(chan struct{})
+
+	// объявляем слайс для данных произвольного типа и передаем в него значения разных типов
+	array := []interface{}{i, s, b, ch}
+
+	// перебираем этот слайс
+	for _, item := range array {
+		// и передаем каждый его элемент в функцию проверки типа
+		checkType(item)
+	}
+
+}
+
+func checkType(x interface{}) {
+	fmt.Println(reflect.TypeOf(x)) // выводим тип переменной x
+}
