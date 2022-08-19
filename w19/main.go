5674a5ed70fc5f90a99e97fc406c08f6c7504b23
Vitaly Bogryakov <vitjrbog@gmail.com>
2022-08-19 11:27:55 +0500

    Completed the 19th assignment

---
+++ w19/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Input: ")
+	var s string
+	_, err := fmt.Scan(&s) // просим пользователя ввести строку в консоль
+	if err != nil {
+		panic(err)
+	}
+
+	r := []rune(s) // переводим строку в руны, чтобы можно было обращаться к символам по индексу
+
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i] // меняем символы местами
+	}
+
+	fmt.Println("Output: " + string(r))
+}
